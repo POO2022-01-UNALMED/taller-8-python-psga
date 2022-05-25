06c7111456911f15b5fe164c02b482165ae05ddc
psga <pusugag@unal.edu.co>
2022-05-25 06:46:26 +0000

    1

---
+++ deportista.py
@@ -0,0 +1,16 @@
+class Deportista:
+    def __init__(self,deporte, a?os):
+        self._deporte = deporte
+        self._a?osPracticando = a?os
+
+    def getDeporte(self):
+        return self._deporte
+
+    def setDeporte(self, deporte):
+        self._deporte = deporte
+
+    def getA?osPracticando(self):
+        return self._a?osPracticando
+
+    def setA?osPracticando(self, a?os):
+        self._arosPracticando = a?os
---
+++ futbolista.py
@@ -0,0 +1,27 @@
+from persona import Persona
+from deportista import Deportista
+
+class Futbolista(Persona,Deportista):
+    listaFutbolistas = []
+    def __init__(self,nombre, edad, altura, sexo, a?os, goles, tarjetas, pierna):
+        Persona.__init__(self,nombre, edad, altura, sexo)
+        Deportista.__init__(self, "Futbol", a?os)
+        self._golesMarcados = goles
+        self._tarjetasRojas = tarjetas
+        self._piernaHabil = pierna
+        Futbolista.listaFutbolistas.append(self)
+    def getTarjetasRojas(self):
+        return self._tarjetasRojas
+    def setTarjetasRojas(self,tarjetas):
+        self._tarjetasRojas = tarjetas
+    def getPiernaHabil(self):
+        return self._piernaHabil
+    def getGolesMarcados(self):
+        return self._golesMarcados
+    def setGolesMarcados(self,goles):
+        self._golesMarcados = goles
+    def setPiernaHabil(self,pierna):
+        self._piernaHabil = pierna
+
+    def __str__(self):
+        return "Mi nombre es " + str(self.getNombre()) + " soy profesional en el deporte " + str(self.getDeporte()) + " Tengo " + str(self.getEdad()) + " a?os de edad y llevo " + str(self.getA?osPracticando()) + " a?os en el deporte"
